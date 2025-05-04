e4a059da56cf300eb35e496a4a31604e2879d010
calemb79 <152223475+calemb79@users.noreply.github.com>
2025-05-04 14:55:47 +0200

    user.js

--- user.js
+++ user.js
@@ -10,7 +10,7 @@ async function login() {
   loginBtn.disabled = true;
 
   try {
-    const res = await fetch("http://localhost:8000/login", {
+    const res = await fetch("https://catering-1.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username: login, password: password })
